c3e50486eeedd71f16745aa2b1b5e980fbf450f0
chesadaphonb-source <chesadaphon.b@ku.th>
2026-07-22 16:36:14 +0700

    Update script.js

--- script.js
+++ script.js
@@ -7,7 +7,7 @@
 const CLIENT_ID = '718318914992-teacpoi09b7ndb4ll22v0rtguoevs55h.apps.googleusercontent.com';
 
 // 2) API_URL: URL ของ Web App ที่ deploy จาก Code.gs (อัปเดตทุกครั้งที่ deploy ใหม่)
-const API_URL = 'https://script.google.com/macros/s/AKfycbymKBeDk1yblZbu4Y9xLlVjILcx3AUzHyj8cuFOGVZDG648RjtPYVb42tIZ9j-MDOJA8A/exec';
+const API_URL = 'https://script.google.com/macros/s/AKfycbw_a56KSdipmhLD0wb6uAlGwupek4cBnv1f6zIiFWIde0PCRiTjWL8TDV_R6bDXPUACIg/exec';
 
 const ALLOWED_DOMAIN = 'ku.th';
 // scope: drive.file (อัปโหลด/จัดการเฉพาะไฟล์ที่แอปนี้สร้าง) + userinfo.email (เอาไว้ตรวจโดเมน)
